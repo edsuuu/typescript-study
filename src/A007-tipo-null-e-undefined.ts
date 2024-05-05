c6094b9cd04dde0c530c9c8ed0c91506f17e320f
edsuu <111332935+edsuuu@users.noreply.github.com>
2024-05-05 18:37:18 -0300

    Create A007-tipo-null-e-undefined.ts

---
+++ src/A007-tipo-null-e-undefined.ts
@@ -0,0 +1,29 @@
+let a;
+if (typeof a === 'undefined') a = 20;
+console.log(a * 2);
+
+export function createPerson (fistName: string, lastName?: string): {
+    fistName: string;
+    lastName?: string;
+} {
+    return {
+        fistName,
+        lastName,
+    };
+}
+
+export function squareOf(x: any) {
+    if (typeof x === 'number') return x * x;
+    return null;
+}
+
+const squareOfTwoNumber = squareOf(2);
+const squareOfTwoString = squareOf('2');
+
+if (squareOfTwoNumber === null) {
+    console.log('Conta inválida');
+} else {
+    console.log(squareOfTwoNumber * 2);
+}
+
+// por exemplo se for fazer uma busca na base de dados, por exemplo algum dado do cliente, entao se nao tiver retorna null para nao dizer q foi um erro de busca ou algum erro no servidor
