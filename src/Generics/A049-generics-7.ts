47f913d8f436fadfb30b40483e5e200f2317b5f7
Edson Lima <111332935+edsuuu@users.noreply.github.com>
2024-05-15 18:43:10 -0300

    Create A049-generics-7.ts

---
+++ src/Generics/A049-generics-7.ts
@@ -0,0 +1,20 @@
+export function isNumber(value: unknown): value is number {
+  return typeof value === 'number';
+}
+console.log(isNumber('123'));
+console.log(isNumber(1));
+
+export function soma<T>(...args: T[]): number | null {
+  const retorno = args.reduce((sum, value) => {
+    if (isNumber(sum) && isNumber(value)) {
+      return sum + value;
+    }
+    return sum;
+  }, 0);
+
+  return retorno;
+}
+
+console.log(soma(1, 2, 3));
+// console.log(soma(...[1, 2, 3, 'a', 'b']));
+console.log(soma('c', 'b', 'a'));
