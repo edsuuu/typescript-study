53d4ee6a1cecd43929e30c96968dec563e4371ba
Edson Lima <111332935+edsuuu@users.noreply.github.com>
2024-05-15 12:13:20 -0300

    Create A037-type-guards.ts

---
+++ src/TiposAvancados/A037-type-guards.ts
@@ -0,0 +1,33 @@
+export function add(a: unknown, b: unknown) {
+  return typeof a === 'number' && typeof b === 'number' ? a + b : `${a}${b}`;
+}
+
+console.log(add(1, 5));
+console.log(add('a', 'b'));
+
+type Pessoa = { tipo: 'pessoa'; nome: string };
+type Animal = { tipo: 'animal'; cor: string };
+
+type PessoaOuAnimal = Pessoa | Animal;
+
+export class Aluno implements Pessoa {
+  tipo = 'pessoa' as const;
+  constructor(public nome: string) {}
+}
+
+export function mostrarNome(obj: PessoaOuAnimal) {
+  //   if ('nome' in obj) console.log(obj.nome);
+  //   if (obj instanceof Aluno) console.log(obj.nome);
+
+  switch (obj.tipo) {
+    case 'pessoa':
+      console.log(obj.nome);
+      return;
+    case 'animal':
+      console.log('Isso é um animal', obj.cor);
+      return;
+  }
+}
+
+mostrarNome(new Aluno('Luiz'));
+mostrarNome({tipo: 'animal', cor: 'rosa'});
